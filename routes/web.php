c21b9791f36a68e7d07293e955869f8dec496179
Luca Siqueira Assis Gorayeb de Mello <lucagorayeb@gmail.com>
2026-04-16 08:44:22 -0400

    -

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,6 @@ use App\Http\Controllers\TesteController;
 use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/teste/{nome}', function ($name) {
-    $data = [
-        'name' => $name,
-        'idade' => 20
-    ];
-    return view('teste', $data);
-});
-
-Route::get('/calcula-idade/{nascimento}/{nome}', [TesteController::class, 'teste']);
-
-<<<<<<< HEAD
-Route::get('/contato/', [ContatoController::class, 'mostrarDataDeHoje'])->name("contato");
-=======
-Route::get('/contato/', [ContatoController::class, 'mostrarDataDeHoje'])->name("contato");
->>>>>>> ff8a8ab084d655de4dc946811501250188d2a0b3
+});*/
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-<<<<<<< HEAD
             input: ['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js'],
-=======
-            input: ['resources/css/app.css', 'resources/js/app.js'],
->>>>>>> ff8a8ab084d655de4dc946811501250188d2a0b3
             refresh: true,
         }),
         tailwindcss(),
