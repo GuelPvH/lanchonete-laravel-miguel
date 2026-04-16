46bf27f246b965415dcf0e00af45640bb316db79
Miguel Carrilho <miguelcleiton25@outlook.com>
2026-04-16 09:54:06 -0400

    Finalizando atualizações da lanchonete

--- routes/web.php
+++ routes/web.php
@@ -5,40 +5,44 @@ use App\Modules\Cliente\ClienteController;
 use App\Modules\Pedido\PedidoController;
 use Illuminate\Support\Facades\Route;
 
-/*
+// ROTAS DE VIEWS
 Route::get('/', function () {
-    return view('')->name('');
-});
+    return view('cardapio', ['produtos' => \App\Models\Produto::all()]);
+})->name('cardapio.index');
 
-Route::post('', function () {
-    return view('')->name('');
-});
+Route::get('/produto/{id}', function ($id) {
+    return view('produto', ['produto' => \App\Models\Produto::find($id)]);
+})->name('produto.show');
 
-Route::put('', function () {
-    return view('')->name('');
-});
+Route::get('/pedido', function () {
+    return view('pedido');
+})->name('pedido.ver');
 
-Route::delete('', function () {
-    return view('')->name('');
-});
-*/
+Route::get('/cliente', function () {
+    return view('cliente');
+})->name('cliente.index');
+
+Route::get('/perfil', function () {
+    return view('perfil');
+})->name('perfil.index');
 
 // ROTAS PEDIDOS
 
-Route::post('/pedidos', [PedidoController::class, 'salvarPedido'])->name('');
-Route::delete('/pedidos/{pedido}', [PedidoController::class, 'deletarPedido'])->name('');
-Route::put('/pedidos/{pedido}', [PedidoController::class, 'alterarPedido'])->name('');
-Route::get('/pedidos/{id}/total', [PedidoController::class, 'calcularTotal'])->name('');
-
+Route::post('/pedidos', [PedidoController::class, 'salvarPedido'])->name('pedido.salvar');
+Route::delete('/pedidos/{pedido}', [PedidoController::class, 'deletarPedido'])->name('pedido.deletar');
+Route::put('/pedidos/{pedido}', [PedidoController::class, 'alterarPedido'])->name('pedido.alterar');
+Route::get('/pedidos/{id}/total', [PedidoController::class, 'calcularTotal'])->name('pedido.total');
+Route::post('/pedidos/adicionar', [PedidoController::class, 'salvarItemPedido'])->name('pedido.adicionar');
+Route::post('/pedidos/finalizar', [PedidoController::class, 'salvarPedido'])->name('pedido.finalizar');
 
 // ROTAS PRODUTOS
 
-Route::post('/produtos', [PedidoController::class, 'salvarProduto'])->name('');
-Route::delete('/produtos/{produto}', [PedidoController::class, 'deletarProduto'])->name('');
-Route::put('/produtos/{produto}', [PedidoController::class, 'alterarProduto'])->name('');
+Route::post('/produtos', [ProdutoController::class, 'salvarProduto'])->name('produto.salvar');
+Route::delete('/produtos/{produto}', [ProdutoController::class, 'deletarProduto'])->name('produto.deletar');
+Route::put('/produtos/{produto}', [ProdutoController::class, 'alterarProduto'])->name('produto.alterar');
 
 // ROTAS CLIENTE
 
-Route::post('/clientes', [PedidoController::class, 'salvarProduto'])->name('');
-Route::delete('/clientes/{cliente}', [PedidoController::class, 'deletarProduto'])->name('');
-Route::put('/clientes/{cliente}', [PedidoController::class, 'alterarProduto'])->name('');
+Route::post('/clientes', [ClienteController::class, 'salvarCliente'])->name('cliente.entrar');
+Route::delete('/clientes/{cliente}', [ClienteController::class, 'deletarCliente'])->name('cliente.deletar');
+Route::put('/clientes/{cliente}', [ClienteController::class, 'alterarCliente'])->name('cliente.alterar');
